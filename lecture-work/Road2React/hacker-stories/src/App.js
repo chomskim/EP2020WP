d0de62c5e501ac84c7629798fc5c246e0d4e7458
chomskim <csknova@gmail.com>
2020-10-12 11:15:31 +0900

    road2react

---
+++ lecture-work/Road2React/hacker-stories/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./App.css";
+
+const title = "My Hacker Stories";
+const stories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: "AWS",
+    url: "https://localhost/",
+    author: "CS Kim",
+    num_comments: 2,
+    points: 5,
+    objectID: 100,
+  },
+];
+
+function App() {
+  const handleChange = (event) => {
+    console.log(event.target);
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Hello {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <hr />
+      <List list={stories} dummy={'hello'} />
+    </div>
+  );
+}
+
+const List = (props) => {
+  console.log(props);
+  return (
+    <ol>
+      {props.list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default App;
